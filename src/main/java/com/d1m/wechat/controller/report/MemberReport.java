c149a9833307f7ee4e85007e8d72056155cc687a
zhenglei.wang <zhenglei.wang@d1m.cn>
2017-09-21 13:47:43 +0800

    ! fix member export memberTags is null bug

--- src/main/java/com/d1m/wechat/controller/report/MemberReport.java
+++ src/main/java/com/d1m/wechat/controller/report/MemberReport.java
@@ -36,15 +36,16 @@ public class MemberReport extends AbstractExcelView {
 		HSSFSheet sheet = workbook.createSheet(name);
 		sheet.setDefaultColumnWidth(11);
 		HSSFRow title = sheet.createRow(0);
-		String[] keys={"no", "nickname", "gender", "mobile", "province", "city", "subscribe.status", "bind.status",
-				"subscribe.time", "group.message.sent", "tag"};
+		String[] keys = { "no", "nickname", "gender", "mobile", "province",
+				"city", "subscribe.status", "bind.status", "subscribe.time",
+				"group.message.sent", "tag" };
 		String[] titleVal = I18nUtil.getMessage(keys, locale);
 		for (int i = 0; i < titleVal.length; i++) {
 			title.createCell(i).setCellValue(titleVal[i]);
 		}
 		SimpleDateFormat df = null;
 
-		if(memberDtoList!=null) {
+		if (memberDtoList != null) {
 			int rowId = 1;
 			for (int i = 0; i < memberDtoList.size(); i++) {
 				MemberDto temp = memberDtoList.get(i);
@@ -52,8 +53,10 @@ public class MemberReport extends AbstractExcelView {
 				dataRow.setHeight((short) 600);
 				dataRow.createCell(0).setCellValue(rowId);
 				dataRow.createCell(1).setCellValue(temp.getNickname());
-				if(temp.getSex() != null){
-					dataRow.createCell(2).setCellValue(I18nUtil.getMessage(Sex.getByValue(temp.getSex()).name().toLowerCase(), locale));
+				if (temp.getSex() != null) {
+					dataRow.createCell(2).setCellValue(
+							I18nUtil.getMessage(Sex.getByValue(temp.getSex())
+									.name().toLowerCase(), locale));
 				}
 				dataRow.createCell(3).setCellValue(temp.getMobile());
 				dataRow.createCell(4).setCellValue(temp.getProvince());
@@ -67,32 +70,41 @@ public class MemberReport extends AbstractExcelView {
 						attentionStatus = "unsubscribe";
 					}
 				}
-				dataRow.createCell(6).setCellValue(I18nUtil.getMessage(attentionStatus, locale));
-				
-				if(temp.getBindStatus() != null && temp.getBindStatus() == 1){
-					dataRow.createCell(7).setCellValue(I18nUtil.getMessage("bind", locale));
-				}else{
-					dataRow.createCell(7).setCellValue(I18nUtil.getMessage("unbind", locale));
+				dataRow.createCell(6).setCellValue(
+						I18nUtil.getMessage(attentionStatus, locale));
+
+				if (temp.getBindStatus() != null && temp.getBindStatus() == 1) {
+					dataRow.createCell(7).setCellValue(
+							I18nUtil.getMessage("bind", locale));
+				} else {
+					dataRow.createCell(7).setCellValue(
+							I18nUtil.getMessage("unbind", locale));
 				}
 
 				df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-				if(temp.getIsSubscribe() && temp.getSubscribeAt() != null){
+				if (temp.getIsSubscribe() && temp.getSubscribeAt() != null) {
 					String attentionTime = df.format(temp.getSubscribeAt());
 					dataRow.createCell(8).setCellValue(attentionTime);
 				}
-				dataRow.createCell(9).setCellValue(temp.getBatchsendMonth()==null?0:temp.getBatchsendMonth());
+				dataRow.createCell(9).setCellValue(
+						temp.getBatchsendMonth() == null ? 0 : temp
+								.getBatchsendMonth());
 				StringBuffer tags = new StringBuffer();
-				for (MemberTagDto mt : temp.getMemberTags()) {
-					tags.append(mt.getName()).append(" | ");
+				if (temp.getMemberTags() != null
+						&& !temp.getMemberTags().isEmpty()) {
+					for (MemberTagDto mt : temp.getMemberTags()) {
+						tags.append(mt.getName()).append(" | ");
+					}
 				}
 				dataRow.createCell(10).setCellValue(tags.toString());
-				/*String unsubscribeTime = null;
-				if (!temp.getIsSubscribe() && temp.getUnsubscribeAt() != null) {
-					unsubscribeTime = df.format(temp.getUnsubscribeAt());
-				}
-				dataRow.createCell(8).setCellValue(unsubscribeTime);
-				dataRow.createCell(11).setCellValue(temp.getFromwhere());
-				dataRow.createCell(12).setCellValue(temp.getActivity());*/
+				/*
+				 * String unsubscribeTime = null; if (!temp.getIsSubscribe() &&
+				 * temp.getUnsubscribeAt() != null) { unsubscribeTime =
+				 * df.format(temp.getUnsubscribeAt()); }
+				 * dataRow.createCell(8).setCellValue(unsubscribeTime);
+				 * dataRow.createCell(11).setCellValue(temp.getFromwhere());
+				 * dataRow.createCell(12).setCellValue(temp.getActivity());
+				 */
 				rowId++;
 			}
 		}
