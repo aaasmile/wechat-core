87ed67132df8ed7cf957136ee09c4e4e1e183c63
Stoney Liu <stoney.liu@d1m.cn>
2017-12-22 16:35:59 +0800

    ! try to export xlsx

--- src/main/java/com/d1m/wechat/controller/report/ReportXlsxStreamView.java
+++ src/main/java/com/d1m/wechat/controller/report/ReportXlsxStreamView.java
@@ -4,6 +4,7 @@ import com.d1m.wechat.util.DateUtil;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import java.util.Map;
  * 通用基于流式处理的Excel导出类
  * Created by Stoney.Liu on 2017/12/20.
  */
-public class ReportXlsxStreamView extends AbstractExcelView {
+public class ReportXlsxStreamView extends AbstractXlsxView {
     private CellProcessor cellProcessor = null;
     private String filename = null;
 
@@ -24,12 +25,12 @@ public class ReportXlsxStreamView extends AbstractExcelView {
     }
 
     @Override
-    protected void buildExcelDocument(Map<String, Object> map, HSSFWorkbook hssfWorkbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    protected void buildExcelDocument(Map<String, Object> map, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         if(cellProcessor!=null){
-            cellProcessor.process(map,hssfWorkbook,httpServletRequest,httpServletResponse);
+            cellProcessor.process(map,workbook,httpServletRequest,httpServletResponse);
 
             // 设置文件名
-            filename = filename + "_" + DateUtil.getCurrentyyyyMMddHHmmss() + ".xls";
+            filename = filename + "_" + DateUtil.getCurrentyyyyMMddHHmmss() + ".xlsx";
             if (httpServletRequest.getHeader("User-Agent").toLowerCase().indexOf("firefox") > 0) {
                 filename = new String(filename.toString().getBytes("utf-8"),"iso-8859-1");
             } else {
