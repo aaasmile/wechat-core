47b0cd39328d5a86f38ee77c5b66566d7a6d3ab2
hcl <hcl.hua@d1m.cn>
2018-06-07 14:29:19 +0800

    wechat-core升级token server

--- src/main/java/com/d1m/wechat/wechatclient/WechatTokenRestService.java
+++ src/main/java/com/d1m/wechat/wechatclient/WechatTokenRestService.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author f0rb on 2017-04-05.
  */
 @FeignClient(
-        name = "wechat-token-server",
+        name = "wechat-token-server-v1",
         configuration = FeignClientConfiguration.class
 )
 public interface WechatTokenRestService {
