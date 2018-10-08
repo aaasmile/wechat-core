f42c8a5b103ed15de7757d990a29519cb3629c35
weilin.liu <weilin.liu@d1m.cn>
2018-10-08 10:26:12 +0800

    取消拉取数据前查库limit限制

--- src/main/java/com/d1m/wechat/controller/member/MemberController.java
+++ src/main/java/com/d1m/wechat/controller/member/MemberController.java
@@ -96,7 +96,7 @@ public class MemberController extends BaseController {
             String msg = "";
             do {
                 //List<MemberDto> memberDtoList=memberService.searchBySql(wechatId,"is_subscribe =1 and nickname is null and (local_head_img_url is null or local_head_img_url ='') limit 0,1 ");
-                List<MemberDto> memberDtoList = memberService.searchBySql(wechatId, "is_subscribe =1 limit 0,1");
+                List<MemberDto> memberDtoList = memberService.searchBySql(wechatId, "is_subscribe =1");
                 Date current = new Date();
                 if (memberDtoList.size() > 0) {
                     MemberDto memberDto = memberDtoList.get(0);
