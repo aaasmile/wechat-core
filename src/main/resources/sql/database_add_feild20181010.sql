2126e373e1c606b4a8170731f96f15859c8d6d88
weilin.liu <weilin.liu@d1m.cn>
2018-10-19 15:03:06 +0800

    去掉加字段字符集编码

--- src/main/resources/sql/database_add_feild20181010.sql
+++ src/main/resources/sql/database_add_feild20181010.sql
@@ -1,4 +1,4 @@
 -- 新增用户关注的渠道来源、二维码扫码场景和二维码扫码场景描述三个字段 ---
-ALTER TABLE member ADD COLUMN `subscribe_scene` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户关注的渠道来源';
+ALTER TABLE member ADD COLUMN `subscribe_scene` varchar(50)  DEFAULT NULL COMMENT '用户关注的渠道来源';
 ALTER TABLE member ADD COLUMN `qr_scene` int(11) DEFAULT NULL COMMENT '二维码扫码场景';
-ALTER TABLE member ADD COLUMN `qr_scene_str` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '二维码扫码场景描述';
+ALTER TABLE member ADD COLUMN `qr_scene_str` varchar(255)  DEFAULT NULL COMMENT '二维码扫码场景描述';
