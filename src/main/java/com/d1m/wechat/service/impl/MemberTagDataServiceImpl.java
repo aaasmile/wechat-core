fa7948d1d8168af75c64295adffe898afda18e98
weilin.liu <weilin.liu@d1m.cn>
2018-11-13 13:38:34 +0800

    标签导入调度任务列表查询

--- src/main/java/com/d1m/wechat/service/impl/MemberTagDataServiceImpl.java
+++ src/main/java/com/d1m/wechat/service/impl/MemberTagDataServiceImpl.java
@@ -7,7 +7,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.d1m.common.ds.TenantContext;
 import com.d1m.common.ds.TenantHelper;
-import com.d1m.wechat.Handler.VerifyHandler;
 import com.d1m.wechat.domain.entity.MemberTagCsv;
 import com.d1m.wechat.domain.entity.MemberTagData;
 import com.d1m.wechat.exception.BatchAddTagException;
