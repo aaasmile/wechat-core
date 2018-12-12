db25565f8103dcfe27a9bdf80ada1ecd722037f2
Joe He <joe.he@d1m.cn>
2018-12-12 17:29:45 +0800

    no message

--- src/main/java/com/d1m/wechat/mapper/InterfaceConfigMapper.java
+++ src/main/java/com/d1m/wechat/mapper/InterfaceConfigMapper.java
@@ -6,9 +6,7 @@ import com.d1m.wechat.dto.InterfaceConfigDto;
 import com.d1m.wechat.model.InterfaceConfig;
 import com.d1m.wechat.util.MyMapper;
 import com.github.pagehelper.Page;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface InterfaceConfigMapper extends MyMapper<InterfaceConfig>{
 
 	Page<InterfaceConfigDto> selectItems(Map<String, String> query);
