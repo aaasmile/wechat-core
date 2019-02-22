ff08fb59891918c849004dd520b71a7f3a49740f
jungang <jungang.wang@chejianer.com>
2019-02-22 11:40:15 +0800

    feat: 单元测试接口

--- src/main/java/com/d1m/wechat/controller/TestController.java
+++ src/main/java/com/d1m/wechat/controller/TestController.java
@@ -5,6 +5,9 @@ import com.d1m.wechat.dto.MemberDto;
 import com.d1m.wechat.mapper.MemberMapper;
 import com.d1m.wechat.model.Member;
 import com.d1m.wechat.util.Security;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -38,6 +41,59 @@ public class TestController {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    public static final String subscribe = "{\n" +
+            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
+            "    \"wechatId\": \"11\",\n" +
+            "    \"FromUserName\": \"oNgDLwYYbDLbDBJwImMuhd6RL5AE\",\n" +
+            "    \"CreateTime\": \"1523422781\",\n" +
+            "    \"MsgType\": \"event\",\n" +
+            "    \"Event\": \"subscribe\"\n" +
+            "  }";
+
+    public static final String scan = "{\n" +
+            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
+            "    \"wechatId\": \"11\",\n" +
+            "    \"FromUserName\": \"oNgDLwSm9vyDdAhCz-yfyISNwGfM\",\n" +
+            "    \"CreateTime\": \"1523604229\",\n" +
+            "    \"MsgType\": \"event\",\n" +
+            "    \"Event\": \"SCAN\",\n" +
+            "    \"EventKey\": \"FMJ1\",\n" +
+            "    \"Ticket\": \"gQEL8jwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyQTJ3OTBwUzBmYjExMDAwME0wN0kAAgSCRdBaAwQAAAAA\"\n" +
+            "  }";
+
+    public static final String click = "{\n" +
+            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
+            "    \"wechatId\": \"11\",\n" +
+            "    \"FromUserName\": \"oNgDLwdFG7pP92NhEAZKOFpoTGQ8\",\n" +
+            "    \"CreateTime\": \"1523600899\",\n" +
+            "    \"MsgType\": \"event\",\n" +
+            "    \"Event\": \"CLICK\",\n" +
+            "    \"EventKey\": \"31\"\n" +
+            "  }";
+
+    public static final String userGetCard = "{\n" +
+            "    \"ToUserName\": \" gh_fc0a06a20993 \",\n" +
+            "    \"wechatId\": \"11\",\n" +
+            "    \"FromUserName\": \" oZI8Fj040-be6rlDohc6gkoPOQTQ \",\n" +
+            "    \"CreateTime\": \"1472551036\",\n" +
+            "    \"MsgType\": \" event \",\n" +
+            "    \"Event\": \" user_get_card \",\n" +
+            "    \"CardId\": \" pZI8Fjwsy5fVPRBeD78J4RmqVvBc \",\n" +
+            "    \"IsGiveByFriend\": \"0\",\n" +
+            "    \"UserCardCode\": \" 226009850808 \",\n" +
+            "    \"FriendUserName\": \"  \",\n" +
+            "    \"OuterId\": \"0\",\n" +
+            "    \"OldUserCardCode\": \"  \",\n" +
+            "    \"OuterStr\": \" 12b \",\n" +
+            "    \"IsRestoreMemberCard\": \"0\",\n" +
+            "    \"IsRecommendByFriend\": \"0\",\n" +
+            "    \"UnionId\": \"o6_bmasdasdsad6_2sgVt7hMZOPfL\"\n" +
+            "  }";
+
     private String secret = "fca216d4ebae108ccb26efb1d0a96a36";
 
     @GetMapping("/member")
@@ -57,10 +113,20 @@ public class TestController {
 
     @GetMapping("/sendMessage")
     public BaseResponse sendMessage() {
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "zhang san");
-        map.put("wechatId", "11");
-        amqpTemplate.convertAndSend("INTERFACE_EXCHANGE", "event.message", map);
+
+        final MapType mapType = objectMapper.getTypeFactory().constructMapType(HashMap.class, String.class, String.class);
+
+        Lists.newArrayList(subscribe, scan, click, userGetCard).forEach(e -> {
+            try {
+                final Map<String, String> map = objectMapper.readValue(e, mapType);
+                for (int i = 1; i < 5; i++) {
+                    amqpTemplate.convertAndSend("wechat.notify.event", "event." + map.get("Event"), map);
+                }
+            } catch (IOException e1) {
+                log.error("Error", e1);
+            }
+        });
+
         return BaseResponse.builder()
                 .data(null)
                 .msg("success")
--- src/test/java/com/d1m/wechat/controller/interfaces/InterfaceRabbitMQListenerTest.java
+++ src/test/java/com/d1m/wechat/controller/interfaces/InterfaceRabbitMQListenerTest.java
@@ -17,10 +17,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.d1m.wechat.controller.TestController.*;
+
 /**
  * Created by jone.wang on 2019/2/21.
  * Description:
  */
+@SuppressWarnings("Duplicates")
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CoreApplication.class)
 @Slf4j
@@ -31,54 +34,6 @@ public class InterfaceRabbitMQListenerTest {
         consulProperties.onStartup();
     }
 
-    private String subscribe = "{\n" +
-            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
-            "    \"wechatId\": \"11\",\n" +
-            "    \"FromUserName\": \"oNgDLwYYbDLbDBJwImMuhd6RL5AE\",\n" +
-            "    \"CreateTime\": \"1523422781\",\n" +
-            "    \"MsgType\": \"event\",\n" +
-            "    \"Event\": \"subscribe\"\n" +
-            "  }";
-
-    private String scan = "{\n" +
-            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
-            "    \"wechatId\": \"11\",\n" +
-            "    \"FromUserName\": \"oNgDLwSm9vyDdAhCz-yfyISNwGfM\",\n" +
-            "    \"CreateTime\": \"1523604229\",\n" +
-            "    \"MsgType\": \"event\",\n" +
-            "    \"Event\": \"SCAN\",\n" +
-            "    \"EventKey\": \"FMJ1\",\n" +
-            "    \"Ticket\": \"gQEL8jwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyQTJ3OTBwUzBmYjExMDAwME0wN0kAAgSCRdBaAwQAAAAA\"\n" +
-            "  }";
-
-    private String click = "{\n" +
-            "    \"ToUserName\": \"gh_cb4da662addc\",\n" +
-            "    \"wechatId\": \"11\",\n" +
-            "    \"FromUserName\": \"oNgDLwdFG7pP92NhEAZKOFpoTGQ8\",\n" +
-            "    \"CreateTime\": \"1523600899\",\n" +
-            "    \"MsgType\": \"event\",\n" +
-            "    \"Event\": \"CLICK\",\n" +
-            "    \"EventKey\": \"31\"\n" +
-            "  }";
-
-    private String userGetCard = "{\n" +
-            "    \"ToUserName\": \" gh_fc0a06a20993 \",\n" +
-            "    \"wechatId\": \"11\",\n" +
-            "    \"FromUserName\": \" oZI8Fj040-be6rlDohc6gkoPOQTQ \",\n" +
-            "    \"CreateTime\": \"1472551036\",\n" +
-            "    \"MsgType\": \" event \",\n" +
-            "    \"Event\": \" user_get_card \",\n" +
-            "    \"CardId\": \" pZI8Fjwsy5fVPRBeD78J4RmqVvBc \",\n" +
-            "    \"IsGiveByFriend\": \"0\",\n" +
-            "    \"UserCardCode\": \" 226009850808 \",\n" +
-            "    \"FriendUserName\": \"  \",\n" +
-            "    \"OuterId\": \"0\",\n" +
-            "    \"OldUserCardCode\": \"  \",\n" +
-            "    \"OuterStr\": \" 12b \",\n" +
-            "    \"IsRestoreMemberCard\": \"0\",\n" +
-            "    \"IsRecommendByFriend\": \"0\",\n" +
-            "    \"UnionId\": \"o6_bmasdasdsad6_2sgVt7hMZOPfL\"\n" +
-            "  }";
 
     @Autowired
     private AmqpTemplate amqpTemplate;
@@ -95,7 +50,7 @@ public class InterfaceRabbitMQListenerTest {
             try {
                 final Map<String, String> map = objectMapper.readValue(e, mapType);
                 for (int i = 1; i < 5; i++) {
-                    amqpTemplate.convertAndSend("INTERFACE_EXCHANGE", "event." + map.get("Event"), map);
+                    amqpTemplate.convertAndSend("wechat.notify.event", "event." + map.get("Event"), map);
                 }
             } catch (IOException e1) {
                 log.error("Error", e1);
