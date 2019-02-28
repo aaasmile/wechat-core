4df06c7ca3b2e7421751edc8137d7ca298d282d5
jungang <jungang.wang@chejianer.com>
2019-02-28 14:09:27 +0800

    fix: feign client named wechat-crm add url

--- src/main/java/com/d1m/wechat/wechatclient/WechatCrmRestService.java
+++ src/main/java/com/d1m/wechat/wechatclient/WechatCrmRestService.java
@@ -1,21 +1,20 @@
 package com.d1m.wechat.wechatclient;
 
+import com.d1m.wechat.configure.FeignClientConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.d1m.wechat.configure.FeignClientConfiguration;
-
-@FeignClient(name = "wechat-crm", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "wechat-crm", url = "${wechat-crm.service-name:}", configuration = FeignClientConfiguration.class)
 public interface WechatCrmRestService {
 
-	@RequestMapping(value = "/api/member/{wechatId}/{memberId}/levels", method = RequestMethod.GET)
-	String getMemberLevels(@PathVariable("wechatId") Integer wechatId,
-			@PathVariable("memberId") Integer memberId);
+    @RequestMapping(value = "/api/member/{wechatId}/{memberId}/levels", method = RequestMethod.GET)
+    String getMemberLevels(@PathVariable("wechatId") Integer wechatId,
+                           @PathVariable("memberId") Integer memberId);
 
-	@RequestMapping(value = "/api/member/{wechatId}/{memberId}/status", method = RequestMethod.GET)
-	String getMemberStatus(@PathVariable("wechatId") Integer wechatId,
-			@PathVariable("memberId") Integer memberId);
+    @RequestMapping(value = "/api/member/{wechatId}/{memberId}/status", method = RequestMethod.GET)
+    String getMemberStatus(@PathVariable("wechatId") Integer wechatId,
+                           @PathVariable("memberId") Integer memberId);
 
 }
