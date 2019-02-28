fbf6cbd9b3b767aa175109dd8b8823be85164362
hcl <hcl.hua@d1m.cn>
2019-02-28 17:41:50 +0800

    call back json

---
+++ src/main/java/com/d1m/wechat/model/Text.java
@@ -0,0 +1,21 @@
+package com.d1m.wechat.model;
+
+public class Text {
+
+    private String content;
+
+    public Text() {
+    }
+
+    public Text(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
