cc39985d1e08c6370db1ef8d5ef64b2525eebd0e
hcl <hcl.hua@d1m.cn>
2019-03-05 11:37:11 +0800

    add k8s service

---
+++ src/main/java/com/d1m/wechat/wechatclient/WechatCrmRestK8sService.java
@@ -0,0 +1,21 @@
+package com.d1m.wechat.wechatclient;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.d1m.wechat.configure.FeignClientConfiguration;
+
+@FeignClient(url = "http://wechat-crm", name = "wechat-crm", configuration = FeignClientConfiguration.class)
+public interface WechatCrmRestK8sService {
+
+	@RequestMapping(value = "/api/member/{wechatId}/{memberId}/levels", method = RequestMethod.GET)
+	String getMemberLevels(@PathVariable("wechatId") Integer wechatId,
+      @PathVariable("memberId") Integer memberId);
+
+	@RequestMapping(value = "/api/member/{wechatId}/{memberId}/status", method = RequestMethod.GET)
+	String getMemberStatus(@PathVariable("wechatId") Integer wechatId,
+      @PathVariable("memberId") Integer memberId);
+
+}
