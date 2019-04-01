da37b589e334744df6cad89da04bf82b5d31597e
liutuo <Nickliu@duetin.com>
2019-04-01 17:45:46 +0800

    修改任务名称。

--- src/main/java/com/d1m/wechat/schedule/job/WeChatCommentJob.java
+++ src/main/java/com/d1m/wechat/schedule/job/WeChatCommentJob.java
@@ -25,7 +25,7 @@ import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
-@JobHander(value="weChatCommentJob")
+@JobHander(value="weChatCommentsJob")
 @Component
 public class WeChatCommentJob extends BaseJobHandler {
     @Resource
